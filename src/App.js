0254a55cefd0ac0e5081c5890b7b5d2261f6f515
Anin-Hasan <200921@ku.ac.bd>
2025-01-15 12:36:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// filepath: /E:/Codemen Solutions Ltd/quil-js/src/App.js
+import React from "react";
+import Heading from "./components/Heading";
+import MyComponents from "./components/MyComponents";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Heading />
+      <MyComponents />
     </div>
   );
 }
---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="text-center font-bold my-4 text-xl">
+      Welcome to QuillJs RTF
+    </div>
+  );
+};
+
+export default Heading;
